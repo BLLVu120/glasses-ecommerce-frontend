c1c5100c13ef65bccf877becba0a73cd4fda3a2b
thinhdubai99-sudo <thinhdubai99@gmail.com>
2026-04-24 08:02:40 +0700

    add code dasboard

--- src/features/manager/api/dashboard-api.ts
+++ src/features/manager/api/dashboard-api.ts
@@ -5,3 +5,17 @@ export const dashboardApi = {
   getRevenue: async () =>
     await api.get<DashboardResponse>('/dashboard/revenue').then((res) => res.data.result), // Trả về trực tiếp .result để dùng cho gọn
 };
+import { api } from '@/lib/axios';
+import type { DashboardResponse } from '../types/types';
+
+export const dashboardApi = {
+  getRevenue: async () =>
+    await api.get<DashboardResponse>('/dashboard/revenue').then((res) => res.data.result), // Trả về trực tiếp .result để dùng cho gọn
+};
+import { api } from '@/lib/axios';
+import type { DashboardResponse } from '../types/types';
+
+export const dashboardApi = {
+  getRevenue: async () =>
+    await api.get<DashboardResponse>('/dashboard/revenue').then((res) => res.data.result), // Trả về trực tiếp .result để dùng cho gọn
+};
